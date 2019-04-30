23aa7288862a776d17a226d806157e4a907fe181
Yes197 <yesicaaws@gmail.com>
2019-04-30 12:55:07 -0500

    Added Button, funcion imprimir nombre

---
+++ app.js
@@ -0,0 +1,9 @@
+const botonIngresar = document.getElementById('botonIngresar');
+const nombreUsuario = document.getElementById('name');
+
+function Welcome() {
+    nombre = nombreUsuario.value;
+    console.log(nombre);
+}
+
+//botonUno.addEventListener('click', function(){})
